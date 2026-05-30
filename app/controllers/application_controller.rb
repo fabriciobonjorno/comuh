7aaf00e1c0d18aca1a0699626bfbaabe26111be2
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2026-05-30 17:22:00 -0400

    fix(controller): Guard browser check in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  #allow_browser versions: :modern if Rails.env.production?
+  allow_browser versions: :modern if Rails.env.production?
 
   rescue_from ActiveRecord::RecordNotFound do |e|
     respond_to do |format|
